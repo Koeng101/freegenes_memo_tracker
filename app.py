38484edb3121e965a4ec5c16d5f80c31fd33eb8e
Keoni Gandall <koeng101@gmail.com>
2020-02-05 04:23:35 -0800

    Updated with Bootstrap

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask,request,render_template
+from flask_bootstrap import Bootstrap
 from werkzeug.contrib.fixers import ProxyFix
 from werkzeug.exceptions import BadRequest
 import psycopg2
 import os
 
-
+# Init app
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
+Bootstrap(app)
+
+# Init database
 conn = psycopg2.connect(os.environ.get('DATABASE_URL'))
 cursor = conn.cursor()
 
-@app.route('/memos')
-def get_memos():
-    cmd = """SELECT m.id,m.NAME, 
+@app.route('/')
+def index():
+    cmd = """SELECT m.name, 
                m.priority, 
                m.estimated_gene_count, 
                t.state, 
@@ -28,21 +32,25 @@ def get_memos():
         ORDER  BY last_updated DESC """
     cursor.execute(cmd)
     data = cursor.fetchall()
-    return render_template("memos.html",value=data)
+    return render_template("index.html",value=data)
 
-@app.route('/memos_full')
-def memos_full():
-    cmd = "SELECT id,name,short_description,justification,priority,urgency,estimated_gene_count,memo_link,synmemo_link,doi FROM memos ORDER BY priority DESC"
-    cursor.execute(cmd)
-    data = cursor.fetchall()
-    return render_template("memos_full.html",value=data)
+@app.route('/faq')
+def faq():
+    return render_template("faq.html")
 
-@app.route('/memos/<uuid>')
-def get_memo_name(uuid):
-    cmd = "SELECT m.name,t.state,t.created_at,t.notes FROM memos as m JOIN timestamps AS t ON t.memo = m.id WHERE m.id = %s ORDER BY t.created_at DESC"
-    cursor.execute(cmd, (uuid,))
-    data = cursor.fetchall()
-    return render_template("memo_times.html",value=data)
+#@app.route('/memos_full')
+#def memos_full():
+#    cmd = "SELECT id,name,short_description,justification,priority,urgency,estimated_gene_count,memo_link,synmemo_link,doi FROM memos ORDER BY priority DESC"
+#    cursor.execute(cmd)
+#    data = cursor.fetchall()
+#    return render_template("memos_full.html",value=data)
+#
+#@app.route('/memos/<uuid>')
+#def get_memo_name(uuid):
+#    cmd = "SELECT m.name,t.state,t.created_at,t.notes FROM memos as m JOIN timestamps AS t ON t.memo = m.id WHERE m.id = %s ORDER BY t.created_at DESC"
+#    cursor.execute(cmd, (uuid,))
+#    data = cursor.fetchall()
+#    return render_template("memo_times.html",value=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
